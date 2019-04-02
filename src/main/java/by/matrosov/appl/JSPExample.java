873aaeb2be5e5b9571d7af2ead9f5f40d9409bb8
Andrey Matrosov <matrosovs2008@yandex.ru>
2019-04-02 13:42:26 +0300

    add jsp

---
+++ src/main/java/by/matrosov/appl/JSPExample.java
@@ -0,0 +1,17 @@
+package by.matrosov.appl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//go to http://localhost:8080/java_servlet_war_exploded/
+@WebServlet
+public class JSPExample extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/index.jsp").forward(req,resp);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 1111
+  Date: 02.04.2019
+  Time: 13:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>JSP Example</title>
+</head>
+<body>
+    <a>Hello jsp</a>
+</body>
+</html>
